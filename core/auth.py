ba418c9f1635c852ddabb80f9f7b44e1be04c34f
Ángela Torrealba <angvts12@gmail.com>
2022-10-31 20:09:09 -0400

    Debug log added

--- core/auth.py
+++ core/auth.py
@@ -8,12 +8,9 @@ api_key_header = APIKeyHeader(name="access_token", auto_error=False)
 
 
 async def get_api_key(api_key_header: str = Security(api_key_header)):
-    print("API KEYS")
-    print(settings.API_KEYS)
-    print(api_key_header)
     if api_key_header in settings.API_KEYS:
         return api_key_header   
     else:
         raise HTTPException(
-            status_code=HTTP_403_FORBIDDEN, detail="Could not validate API KEY"
+            status_code=HTTP_403_FORBIDDEN, detail="Could not validate API KEY / " + str(settings.API_KEYS) + " / " + str(api_key_header)
         )
